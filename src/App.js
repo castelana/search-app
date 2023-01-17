992b92e815ce8494c899057df5de771535a9e135
Anastasiia Kichkimet <108686215+castelana@users.noreply.github.com>
2023-01-17 13:05:19 +0100

    added code from sandbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="weather-app">
+        <form className="mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                className="form-control"
+                autocomplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="overview">
+          <h1>New York</h1>
+          <ul>
+            <li>Last updated: Sunday 10:00</li>
+            <li> Cloudy</li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="d-flex weather-temperature">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                alt="Cloudy"
+              />
+              <div>
+                <span className="units">
+                  <a href="/">°C</a>|<a href="/">°F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity:80%</li>
+              <li>Wind:30km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
